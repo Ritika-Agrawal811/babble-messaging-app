bf4214c9e9e2dd69093bcbb43ed3091baf0f9cda
Ritika Agrawal <ritikaagrawal339@gmail.com>
2024-05-18 20:29:03 +0530

    style(emptystate): reduce font size and weight

--- app/_components/EmptyState.tsx
+++ app/_components/EmptyState.tsx
@@ -3,9 +3,7 @@ import clsx from "clsx"
 const EmptyState = () => {
     return (
         <div className={clsx("h-full bg-gray-100", "flex items-center justify-center")}>
-            <h3 className={clsx("text-lg xl:text-xl", "font-semibold text-gray-800")}>
-                Select a chat or start a new conversation
-            </h3>
+            <h3 className={clsx("xl:text-lg", "text-gray-800")}>Select a chat or start a new conversation</h3>
         </div>
     )
 }
